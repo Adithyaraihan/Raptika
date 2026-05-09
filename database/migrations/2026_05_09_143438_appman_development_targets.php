0761e9ba23d3802c1f7665442ab7f52d6c83a53a
Adithya Raihan Pratama <144696680+Adithyaraihan@users.noreply.github.com>
2026-05-09 21:55:42 +0700

    menambhakna tabel appman:application management

--- database/migrations/2026_05_09_143438_appman_development_targets.php
+++ database/migrations/2026_05_09_143438_appman_development_targets.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
-        Schema::create('rekayasa_application_replications', function (Blueprint $table) {
+        Schema::create('appman_development_targets', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('service_type_id');
             $table->foreignId('service_type_id')
                 ->constrained('service_types')
                 ->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignId('institution_id')->constrained('general_institution_categories');
             $table->tinyInteger('month');
             $table->smallInteger('year');
-            $table->integer('total_replications')->default(0);
+            $table->integer('outside_dc_jabar');
+            $table->integer('manual_service');
+
             $table->timestamps();
         });
     }
     public function down(): void
     {
-        Schema::dropIfExists('rekayasa_application_replications');
+        Schema::dropIfExists('appman_development_targets');
     }
 };
--- database/migrations/2026_05_09_144216_appman_drive_jabar_stats.php
+++ database/migrations/2026_05_09_144216_appman_drive_jabar_stats.php
@@ -7,21 +7,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('rekayasa_application_replications', function (Blueprint $table) {
+        Schema::create('appman_drive_jabar_stats', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('service_type_id');
             $table->foreignId('service_type_id')
                 ->constrained('service_types')
                 ->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignId('institution_id')->constrained('general_institution_categories');
             $table->tinyInteger('month');
             $table->smallInteger('year');
-            $table->integer('total_replications')->default(0);
+            $table->integer('total_users');
             $table->timestamps();
         });
     }
     public function down(): void
     {
-        Schema::dropIfExists('rekayasa_application_replications');
+        Schema::dropIfExists('appman_drive_jabar_stats');
     }
 };
